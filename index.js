6714a469ebddae388a3162c6ebc97deb733894bd
maazkhan77 <maazkhan7769@gmail.com>
2023-12-06 14:50:15 +0530

    file system module

--- index.js
+++ index.js
@@ -1,27 +1,22 @@
-const fs = require("fs");
+const fsPromises = require("fs").promises;
 const path = require('path')
 
-fs.readFile(path.join(__dirname, 'files', 'starter.txt'), "utf8", (err, data) => {
-  if (err) throw err;
-  console.log(data);
-});
+const fileOps = async () => {
+    try {
+        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
+        console.log(data)
+        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
+        await fsPromises.writeFile(path.join(__dirname, 'files', 'reply.txt'), "This is reply text")
+        await fsPromises.appendFile(path.join(__dirname, 'files', 'reply.txt'), "\n\n Ok I understand")
+        await fsPromises.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'))
+        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'newReply.txt'), 'utf8')
+        console.log(newData)
+    } catch (error) {
+        console.error(error);
+    }
+}
 
-console.log("first");
-
-fs.writeFile(path.join(__dirname, 'files', 'reply.txt'), "from witefile function", (err) => {
-    if (err) throw err;
-    console.log("operation complete");
-
-    fs.appendFile(path.join(__dirname, 'files', 'reply.txt'), "\n\n updated from append file", (err) => {
-        if (err) throw err;
-        console.log("appended");
-
-        fs.rename(path.join(__dirname, 'files', 'reply.txt'),path.join(__dirname, 'files', 'newReply.txt') , (err) => {
-            if (err) throw err;
-            console.log("rename complete");
-          });
-      });
-  });
+fileOps()
 
 //exit on uncaught errors
 process.on("uncaughtException", (err) => {
