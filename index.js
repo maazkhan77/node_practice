15b196459e3ff8563c01a1e29ed0788f3f02f406
maazkhan77 <maazkhan7769@gmail.com>
2023-12-06 16:46:58 +0530

    custom event logger function

--- index.js
+++ index.js
@@ -1,25 +1,13 @@
-const fsPromises = require("fs").promises;
-const path = require('path')
+const logEvents = require("./logEvents");
 
-const fileOps = async () => {
-    try {
-        const data = await fsPromises.readFile(path.join(__dirname, 'files', 'starter.txt'), 'utf8')
-        console.log(data)
-        await fsPromises.unlink(path.join(__dirname, 'files', 'starter.txt'))
-        await fsPromises.writeFile(path.join(__dirname, 'files', 'reply.txt'), "This is reply text")
-        await fsPromises.appendFile(path.join(__dirname, 'files', 'reply.txt'), "\n\n Ok I understand")
-        await fsPromises.rename(path.join(__dirname, 'files', 'reply.txt'), path.join(__dirname, 'files', 'newReply.txt'))
-        const newData = await fsPromises.readFile(path.join(__dirname, 'files', 'newReply.txt'), 'utf8')
-        console.log(newData)
-    } catch (error) {
-        console.error(error);
-    }
-}
+const EventEmitter = require("events");
 
-fileOps()
+class MyEmitter extends EventEmitter {}
 
-//exit on uncaught errors
-process.on("uncaughtException", (err) => {
-  console.error(`There was an uncaught error: ${err}`);
-  process.exit(1);
-});
+const myEmitter = new MyEmitter();
+
+myEmitter.on("log", (msg) => logEvents(msg));
+
+setTimeout(() => {
+  myEmitter.emit("log", "Log event emitted!");
+}, 2000);
