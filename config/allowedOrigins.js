93f8ae88bc1f3b310b74468479c35cd7d3fa6bcd
maazkhan77 <maazkhan7769@gmail.com>
2023-12-13 13:30:41 +0530

    added localhost in the allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,8 @@
 const allowedOrigins = [
   "http://127.0.0.1:3000",
   "http://127.0.0.1:5500",
-  "https://mock-backend-nsgr.onrender.com"
+  "https://mock-backend-nsgr.onrender.com",
+  "http://localhost:3000"
 ];
 
 module.exports = allowedOrigins;
