e93314196ec77a379206c940e4082f57f7bf75c0
maazkhan77 <maazkhan7769@gmail.com>
2023-12-14 15:36:45 +0530

    Bug fixes in refreshTokenController

--- controllers/refreshTokenController.js
+++ controllers/refreshTokenController.js
@@ -5,7 +5,9 @@ const handleRefreshToken = async (req, res) => {
   const cookies = req.cookies;
   if (!cookies?.jwt) return res.sendStatus(401);
   const refreshToken = cookies.jwt;
-  const foundUser = User.findOne({ refreshToken }).exec();
+  console.log(refreshToken)
+  const foundUser = await User.findOne({ refreshToken }).exec();
+  console.log(foundUser)
   if (!foundUser) return res.sendStatus(403); //forbidden
   // evaluate jwt
   jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
